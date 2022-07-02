332d2585538e344b32a96868dce32e0f8aee03b0
Theo Browne <me@t3.gg>
2022-07-01 22:36:11 -0700

    init

---
+++ src/routes/todos/index.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	type Todo = {
+		uid: string;
+		created_at: Date;
+		text: string;
+		done: boolean;
+		pending_delete: boolean;
+	};
+
+	export let todos: Todo[];
+
+	console.log('todos?', todos);
+</script>
+
+<svelte:head>
+	<title>Todos</title>
+	<meta name="description" content="A todo list app" />
+</svelte:head>
+
+<div class="todos">
+	<h1>Todos</h1>
+	<div>
+		<h2>
+			Request-to-paint time: <span id="p-full-time" />
+			ms
+		</h2>
+		<a href="/">Back</a>
+		<script>
+			const currentTime = new Date();
+
+			// round trip time
+			const fullTime = currentTime - window.performance.timing.requestStart;
+			document.getElementById('p-full-time').innerHTML = fullTime;
+		</script>
+	</div>
+</div>

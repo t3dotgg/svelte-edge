30f5eb02962611b62ab34a61c02a546467f1b2b9
Theo Browne <me@t3.gg>
2022-07-02 22:40:47 -0700

    cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,12 +24,6 @@
 	import Card from '$lib/Card.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import { page } from '$app/stores';
-
-	/** @type {string} */
-	export let city;
-
-	/** @type {string} */
-	export let ip;
 </script>
 
 <svelte:head>
@@ -44,9 +38,12 @@
 	<meta name="twitter:image:alt" content="The Vercel and Svelte logos" />
 </svelte:head>
 
-<Card />
-
 <main>
+	<img
+		src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg"
+		style="width: 48px;"
+	/>
+	<h1>Theo's Crappy Benchmark (Svelte On Vercel Edge)</h1>
 	<h2>Svelte likes to override my script so check console for real #s</h2>
 	<h2><span>Full request to render time (according to Svelte):{performance()}ms</span></h2>
 	<h2><span>Full request to render time (according to Theo): <span id="overrideme" />ms</span></h2>
@@ -57,23 +54,7 @@
 		document.getElementById('overrideme').innerHTML = fullTime;
 		console.log('THEO REPORTS', fullTime);
 	</script>
-	<div class="info">
-		<div class="block">
-			<div class="contents">
-				<span>Your city</span>
-				<strong>{city}</strong>
-			</div>
-		</div>
-		<div class="block">
-			<div class="contents">
-				<span>Your IP address</span>
-				<strong>{ip}</strong>
-			</div>
-		</div>
-	</div>
 </main>
-
-<Footer />
 
 <style>
 	main {
@@ -82,95 +63,5 @@
 		height: 100%;
 		padding: 2.5rem;
 		box-sizing: border-box;
-	}
-
-	h1 {
-		font-size: min(16vw, 4rem);
-		font-weight: 600;
-		margin: 0;
-		text-align: center;
-		position: absolute;
-		top: 10vh;
-		left: 0;
-		width: 100%;
-		padding: 0 2.5rem;
-		box-sizing: border-box;
-		line-height: 1;
-	}
-
-	h1 span {
-		display: block;
-		max-width: 6em;
-		margin: 0 auto;
-	}
-
-	.info {
-		--border-color: rgba(0, 0, 0, 0.08);
-		position: absolute;
-		bottom: 9rem;
-		left: 0;
-		width: 100%;
-		display: grid;
-		padding: 0 2.5rem;
-		font-size: min(5vw, 2rem);
-		box-sizing: border-box;
-	}
-
-	.block {
-		display: flex;
-		margin: 0;
-		padding: 1rem 0;
-	}
-
-	.block:nth-child(1) {
-		border-bottom: 1px solid var(--border-color);
-	}
-
-	.block span {
-		display: block;
-		margin: 0 0 0.2em 0;
-		text-transform: uppercase;
-		font-size: 0.6em;
-	}
-
-	.block strong {
-		display: block;
-		font-size: 1.4em;
-		margin: 0;
-		font-weight: 600;
-	}
-
-	@media (min-width: 640px) {
-		h1 {
-			font-size: min(8vw, 4rem);
-		}
-
-		h1 span {
-			max-width: none;
-		}
-
-		.info {
-			display: flex;
-			justify-content: center;
-			position: relative;
-			top: 60vh;
-			gap: 4rem;
-			grid-template-columns: 1fr 1fr;
-		}
-
-		.block:nth-child(1) {
-			border-bottom: none;
-			justify-content: end;
-		}
-
-		.block:nth-child(2) {
-			justify-content: start;
-		}
-	}
-
-	@media (prefers-color-scheme: dark) {
-		.block:nth-child(1) {
-			--border-color: rgba(255, 255, 255, 0.35);
-		}
 	}
 </style>
